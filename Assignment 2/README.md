8d41b99bc469f794e611f39684dc8787ac4455e2
Daivik Goel <daivikgoel98@gmail.com>
2021-07-06 15:08:32 -0400

    .

---
+++ Assignment 2/README.md
@@ -0,0 +1,15 @@
+# ECE 458 assignment 2 Code
+
+```pip3 install sympy```
+
+```python3 assignment2.py```
+
+uncomment any print statements that might be necessary
+
+Select question to be run (1-5) 
+
+for example:
+> WHICH QUESTION DO YOU WANT: 5
+
+
+In question 2, the user is expected to input an integer to calculate the SHA3-224 value of their input.
--- Assignment 2/assignment2.py
+++ Assignment 2/assignment2.py
@@ -2,6 +2,7 @@ from random import randint
 import hashlib
 import sys
 from sympy import mod_inverse
+import datetime
 
 if sys.version_info < (3, 6):
     import sha3
@@ -19,117 +20,163 @@ sk1 = 432398415306986194693973996870836079581453988813
 sk2 = 165849943586922055423650237226339279137759546603
 sk3 = 627658512551971075308886219669315148725310346887
 
-amnt0, amnt1, amnt2, amnt3 = 1, 2, 3, 4
-
+amnt= [1, 2, 3, 4]
+hashobj = hashlib.sha3_224()
+#primary key array. Will be populated after question1
 pk = []
 
 def generate_public_key(g, sk, p):
-    print("GENRATING PUBLIC KEY")
-    
+    #print("GENRATING PUBLIC KEY")
+    #pow does gives us g ^sk %p 
     output = pow(g, sk, p)
     return output
 
 def question1(g, p, sks): 
     for sk in sks:  
+        #goes through all secret keys and generates public key
         pki = generate_public_key(g, sk, p)
         pk.append(pki)
         #print(sk, pki)
     #print(pk)
 
-def question2():
-    
-    hashobj = hashlib.sha3_224()
-    
-    
-    # providing the input to the hashing algorithm.
-
-    integer = int(input("PLEASE ENTER YOUR INTEGER: "))
+def question2(integer):
+    #converts integer into int and then hex. We then remove the leading zeroes in the hex. 
+    integer = int(integer)
     integer = hex(integer)
     integer = integer[2:]
     integer = bytes(integer, encoding='utf-8')
-
+    #From there we encode it into bytes and hash the integer
     hashobj.update(integer)
     hashed = str(hashobj.hexdigest())
 
-    print(bin(int(hashed, 16))[2:])
+    #print(bin(int(hashed, 16))[2:])
+
+    #we return the value in binary
+    return bin(int(hashed, 16))[2:]
 
 def question3(pk1, pk2, amnt):
-    generate_message(pk1,pk2,amnt)
+    #calls the generate message function to generate the concatenated message
+    print(generate_message(pk1,pk2,amnt))
 
 
 def generate_signature(message, p, q, g, sk):
+    #get random k in between 0 and p
     k = randint(0, p-1)
 
-    hashobj = hashlib.sha3_224()
-    # hashed_num = int.from_bytes(hashlib.sha3_224(message.encode()).digest(), byteorder='big' )
 
-
+    #convert message into hex and then bytes
     integer = hex(message)
     integer = integer[2:]
     integer = bytes(integer, encoding='utf-8')
 
+    #hash the message
     hashobj.update(integer)
     hashed = str(hashobj.hexdigest())
-    hashed_num = int(hashed, 16)
-
-    print("hashed_num ", hashed_num)
+    #convert from hex string to int
+    hashed_num = int(hashed, 16) #this is our h(m)
     
-    # secret key sk1 ~ x
-    print('secret key ', sk)
-    y = pow(g, sk, p) # public key
-    print('public key ', y)
 
-    r = pow(g, k, p)
+    
+    y = pow(g, sk, p) # public key
+    #print('public key ', y)
+
+    r = pow(g, k, p) #first integer of signature paur
 
 
     # hashed_num = (sk1 * r + k * s) % q
-    s = ((hashed_num - sk * r)  * mod_inverse(k, q)) % q
+    s = ((hashed_num - sk * r)  * mod_inverse(k, q)) % q #second half of signature pair
 
-    print((hashed_num - sk * r)  * mod_inverse(k, q))
+    #print((hashed_num - sk * r)  * mod_inverse(k, q))
     print('r ', r)
     print('s ', s)
+    #send signature pair and publick key abck 
     return hashed_num, r, s, y
 
-    # ex:
-    # print("s ", ((22 - 5 * 3) * mod_inverse(8, 11)) % 11)
-
-    # obj_sha3_224 = hashlib.sha3_224(message.encode()).digest()
-    # print("sha3_224 hash in hex: ", obj_sha3_224)
 
 
 def verify_signature(hashed_message, r, s, g, p, q, pk):
+    #check if values fall in expected rangers
     if r < 0 or r > p or s < 0 or s > p:
         print ("verification rejected ")
         return False
 
-    # u = hashed_num * s inverse mod q, 
-    # and v = -r * s inverse mod q
+
+
     s1 = mod_inverse(s, q)
-    u = hashed_message * mod_inverse(s, q) % q
-    v = (-r * s1) % q
+    u = hashed_message * mod_inverse(s, q) % q    # u = hashed_num * s inverse mod q, 
+    v = (-r * s1) % q    # and v = -r * s inverse mod q
 
-    print("u: ", u)
-    print("v: ", v)
-
-    arg1 = pow(g, u, p)
-    arg2 = pow(pk, v, p)
+    arg1 = pow(g, u, p) #g ^ u mod p
+    arg2 = pow(pk, v, p) #pk ^ v mod p
 
     w = arg1 * arg2
-    w = pow(w, 1, p)
+    w = pow(w, 1, p) # g^u y^v mod p 
+    #this value is our generated r
     print("w", w)
-
+    
+    #if the r is the same we can verify that we generated a valid signature
     if r == w:
         print("verification succesful ")
         return True
 
 
+def find_nounce():
+    found = 0
+
+    NONCE = 0
+
+    question1(g,p,sks) #generate the public keys
+
+    messages = [] #save m1, m2, m3 etc
+    hashed_messages = [question2(amnt[0])] #Offset by 1 as h(m0) is h(amnt0)
+
+    for i in range(len(pk)-1):
+        #create a message using primary keys
+        messages.append(generate_message(pk[i], pk[i+1], amnt[i+1]))
+        #create hashed message using these messages
+        hashed_messages.append(question2(generate_message(pk[i], pk[i+1], amnt[i+1])))
+    nonces = []
+
+    #use this to determine length of time it took
+    start_time = datetime.datetime.now()
+
+    for i in range(2):
+        #find nonce1 and nonce2
+        found = 0
+        NONCE = 0
+        print(i)
+        for y in [26,28,30,32]: #this is for testing purposes on effort required
+            found = 0
+            while found == 0:
+                #because the array is offset by 1 in hashedmessages it represents the equation NONCE + mi + h(m i-1)
+                z = NONCE + messages[i] + int(hashed_messages[i])
+                #find hash of the concatenation. 
+
+                new_hash = question2(z) #new_hash is Ti
+                # since the first 24 bits aren't shown in binary representation, when the length of the new hash is exactly 24 bits short then we know we had 24 leading zeroes
+                if 224 - len(new_hash) == y:
+                    #if we have 24 leading zeroes we have found a nonce
+                    found = 1
+                    end_time = datetime.datetime.now()
+
+                    print("elapsed time for finding nonce: ", (end_time - start_time))
+                    nonces.append(NONCE)
+                NONCE +=1
+            
+    #show both nonces
+    print(nonces)
+
+
+    pass
+
 def generate_message(pk1, pk2, amnt):
+    #take the primary keys, convert to binary keeping significant digits of 399, and then add the amnt to it. We then convert back into an integer
     pk1= bin(pk1)[2:401]
     pk2 = bin(pk2)[2:401]
-    amnt = bin(amnt)[2:] if amnt != 1 else "01"
+    amnt = bin(amnt)[2:] if amnt != 1 else "01" #special case for 1.
 
     m1 = int(pk1 + pk2 + amnt, 2)
-    print(m1)
+    return m1
 
 def selector():
     selected = input("WHICH QUESTION DO YOU WANT: ")
@@ -137,16 +184,27 @@ def selector():
     if selected == '1':
         question1(g,p,sks)
     elif selected == '2':
-        question2()
+        question2(input("PLEASE ENTER YOUR INTEGER: "))
     elif selected =='3':
+        #gets messages for both m1 and m2. Question 1 populates primary key array
         question1(g,p,sks)
+        print("M1")
         question3(pk[0], pk[1], 2)
+        print("M2")
+        question3(pk[1], pk[2], 3)
+        
     elif selected =='4':
-        hash_msg, generated_r, generated_s, y = generate_signature(45, p, q, g, sk1)
-        # generate_signature(45, 23, 11, 2, 5)
-
+        #generate public keys
+        question1(g,p,sks)
+        #get digital signature of sk1
+        hash_msg, generated_r, generated_s, y = generate_signature(generate_message(pk[0], pk[1], amnt[1]), p, q, g, sk1)
+        #verify digital signature of sk1
         verify_signature(hash_msg, generated_r , generated_s, g, p, q, y)
+        #get digital signature of sk2
+        hash_msg, generated_r, generated_s, y = generate_signature(generate_message(pk[1], pk[2], amnt[2]), p, q, g, sk2)
+
+    elif selected == '5':
+        find_nounce()
 
 selector()
     
-
